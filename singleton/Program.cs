822b24b8ea8ddd78cad14639bfa58ae1b7ce186c
Jisan Mahmud <jisa.mahmud20@gmail.com>
2026-06-06 16:26:45 +0600

    feat: implement singleton pattern

---
+++ singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class DBConnection
+{
+    private static DBConnection? _instance = null;
+
+    private DBConnection() { }
+
+    public static DBConnection GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new DBConnection();
+        }
+        return _instance;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DBConnection db1 = DBConnection.GetInstance();
+        DBConnection db2 = DBConnection.GetInstance();
+
+        if (object.ReferenceEquals(db1, db2))
+        {
+            Console.WriteLine("Both instances are the same.");
+        }
+        else
+        {
+            Console.WriteLine("Instances are different.");
+        }
+    }
+}
