ef17e9eef2ed561a111c394239b3a9e3e47710cf
Jisan Mahmud <jisa.mahmud20@gmail.com>
2026-06-23 18:14:57 +0600

    feat: Implement chain of responsibility design pattern

---
+++ chain_of_responsibility/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public abstract class Approver
+{
+    protected Approver _nextApprover;
+
+    public void SetNext(Approver approver)
+    {
+        _nextApprover = approver;
+    }
+
+    public abstract void ProcessRequest(decimal amount);
+}
+
+public class Manager : Approver
+{
+    public override void ProcessRequest(decimal amount)
+    {
+        if(amount < 5000)
+        {
+            Console.WriteLine($"Your request accept for amount {amount}.");
+            Console.WriteLine("This request accepted by Mananger!");
+        }
+        else if(_nextApprover != null)
+        {
+            _nextApprover.ProcessRequest(amount);
+        }
+    }
+}
+
+
+public class Director : Approver
+{
+    public override void ProcessRequest(decimal amount)
+    {
+        if(amount < 50000)
+        {
+            Console.WriteLine($"Your request accept for amount {amount}.");
+            Console.WriteLine("This request accepted by Director!");
+        }
+        else if(_nextApprover != null)
+        {
+            _nextApprover.ProcessRequest(amount);
+        }
+    }
+}
+
+public class CEO : Approver
+{
+    public override void ProcessRequest(decimal amount)
+    {
+        Console.WriteLine($"Your request accept for amount {amount}.");
+        Console.WriteLine("Your request accepted by CEO!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Approver manager = new Manager();
+        Approver director = new Director();
+        Approver ceo = new CEO();
+
+        manager.SetNext(director);
+        director.SetNext(ceo);
+
+        manager.ProcessRequest(200000);
+    }
+}
