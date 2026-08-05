b518331625218251df5eb0415813350e1c82b016
Jisan Mahmud <jisa.mahmud20@gmail.com>
2026-08-06 01:35:04 +0600

    feat: implement memento design pattern

---
+++ memento-design-pattern/Program.cs
@@ -0,0 +1,74 @@
+﻿class Memento
+{
+    public string State { get; set; }
+
+    public Memento(string state)
+    {
+        State = state;
+    }
+}
+
+class Document
+{
+    public string Text { get; set; }
+
+    public Memento Save()
+    {
+        return new Memento(Text);
+    }
+
+    public void Restore(Memento memento)
+    {
+        Text = memento.State;
+    }
+}
+
+class History
+{
+    private Stack<Memento> _history = new();
+
+    public void Save(Memento memento)
+    {
+        _history.Push(memento);
+    }
+
+    public Memento Undo()
+    {
+        if (_history.Count <= 1)
+            return null;
+
+        _history.Pop();
+
+        return _history.Peek();
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Document doc = new Document();
+        History history = new History();
+        doc.Text = "Hello";
+        history.Save(doc.Save());
+
+        doc.Text = "Hello world";
+        history.Save(doc.Save());
+
+        doc.Text = "Hello World!!!";
+        history.Save(doc.Save());
+
+        Console.WriteLine("Current Text" + doc.Text);
+
+        // Undo
+        Memento previous = history.Undo();
+
+        while (previous != null)
+        {
+            doc.Restore(previous);
+            Console.WriteLine($"After undo top element = {doc.Text}");
+            previous = history.Undo();
+        }
+    }
+}
