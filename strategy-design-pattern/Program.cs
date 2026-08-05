04d6e616345ce66c1655bd609fc88757752133ae
Jisan Mahmud <jisa.mahmud20@gmail.com>
2026-08-05 14:25:01 +0600

    feat: implement strategy design pattern

---
+++ strategy-design-pattern/Program.cs
@@ -0,0 +1,62 @@
+﻿interface IPaymentStrategy
+{
+    void Pay(double amount);
+}
+
+
+class CreditCardPayment : IPaymentStrategy
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Pay amount {amount} using credit card");
+    }
+}
+
+class PaypalPayment : IPaymentStrategy
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Pay amount {amount} using paypal");
+    }
+}
+
+class BkashPayment : IPaymentStrategy
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Pay amount {amount} using Bkash");
+    }
+}
+
+
+class PaymentContext
+{
+    private IPaymentStrategy _paymentStrategy;
+
+    public void SetPaymentStrategy(IPaymentStrategy paymentStrategy)
+    {
+        _paymentStrategy = paymentStrategy;
+    }
+
+    public void PayAmount(double amount)
+    {
+        _paymentStrategy.Pay(amount);
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PaymentContext paymentContext = new PaymentContext();
+        paymentContext.SetPaymentStrategy(new BkashPayment());
+        paymentContext.PayAmount(1000);
+
+        paymentContext.SetPaymentStrategy(new CreditCardPayment());
+        paymentContext.PayAmount(100);
+
+        paymentContext.SetPaymentStrategy(new PaypalPayment());
+        paymentContext.PayAmount(200);
+    }
+}
